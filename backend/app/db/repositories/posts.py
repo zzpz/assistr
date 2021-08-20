e68e6ba940032e53eb90a8a6fbf3b1aab53380ff
zzpz <michael.wilson1@uqconnect.edu.au>
2021-08-20 20:27:15 +1000

    posts repo layout

---
+++ backend/app/db/repositories/posts.py
@@ -0,0 +1,26 @@
+from app.db.repositories.base import BaseRepository
+
+# from app.models.post import PostCreate, PostInDB, PostUpdate
+
+
+# CREATE_QUERY
+
+# GET_QUERY
+
+
+class PostsRepository(BaseRepository):
+    """
+    All database actions associated with Posts
+    """
+
+    # async def get_(self, *, val:PostCreate) -> PostInDB:
+    #     """
+    #     Queries the database for
+    #     """
+    #     return None
+
+    # async def create_(self, *, val:PostCreate) -> PostInDB:
+    #     """
+    #     creates a user
+    #     """
+    #     return None
