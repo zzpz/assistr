c75568e9470e57ce2121a021b8faf48c359ab6da
zzpz <michael.wilson1@uqconnect.edu.au>
2021-08-23 09:38:06 +1000

    test successful user registration

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -42,16 +42,26 @@ class TestUserRoutes:
 
 class TestUserRegistration:
     async def test_valid_input_creates_user(
-        self, app: FastAPI, client: AsyncClient
+        self, app: FastAPI, client: AsyncClient, db: Database
     ) -> None:
+        # we want to access the database without routing
+        user_repo = UsersRepository(db)
 
         # we will remove this out into conftest
         test_user = UserCreate(
             email="conf@test.com",
             password="password",
             username="conftest",
+            superflous="not used here",
         )
 
+        # this query is not exposed publicly as a route
+        user_in_db = await user_repo.get_user_by_email(email=test_user.email)
+
+        # make sure they don't exist
+        assert user_in_db is None
+
+        # send post request to create and ensure is successful
         # create the json payload for the endpoint
         payload = {"new_user": test_user.dict()}
 
@@ -61,7 +71,16 @@ class TestUserRegistration:
         # assert response is expected
         assert res.status_code == HTTP_201_CREATED
 
-        # assert returned json is expected
+        # ensure now exists in db
+        user_in_db = await user_repo.get_user_by_email(email=test_user.email)
+        assert user_in_db is not None
+        assert user_in_db.email == test_user.email
 
-        # we will use the InDB model instead of the returned json
-        # need to query database / users repo instead TODO
+        # assert returned json response is equal to user in database
+        # tests if we modify it before we send it we don't modify anything incorrectly.
+
+        # create two dicts of the values in the two models and compare dicts
+        returned_user = UserPublic(**res.json()).dict(exclude={""})
+        assert returned_user == user_in_db.dict(exclude={"password", "salt"})
+
+        # a UserPublic is the same as a UserInDB but with less values
