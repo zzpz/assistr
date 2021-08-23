69d67634b2a5f71c67452ef331b0d9c8606711cf
zzpz <michael.wilson1@uqconnect.edu.au>
2021-08-23 10:41:16 +1000

    password auth

---
+++ backend/app/services/authentication.py
@@ -0,0 +1,33 @@
+# Cryptography library
+import bcrypt
+from passlib.context import CryptContext
+
+# Models
+from app.models.user import UserBase, UserPasswordUpdate
+
+# password cryptography attributes
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class AuthService:
+    def hash_pw(self, *, plaintext_password: str) -> UserPasswordUpdate:
+        return UserPasswordUpdate(salt="salt", password="password")
+
+    def generate_salt(self) -> str:
+        return bcrypt.gensalt().decode()
+
+    # note - bcrypt provides its own salt when hashing but we do it anyway
+    # keeps things fresh, gets the people going, highlights what we're doing
+    def hash_password(self, *, password: str, salt: str) -> str:
+        return pwd_context.hash(password + salt)
+
+    def verify_password(
+        self,
+        password: str,
+        salt: str,
+        hashed_pw: str,
+    ) -> bool:
+        """
+        Use the hashed password stored in db to compare to provided password. Salt is combined with provided pw to generate hash for comparison.
+        """
+        return pwd_context.verify(password + salt, hashed_pw)
