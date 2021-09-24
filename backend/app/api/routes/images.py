32654ed8024f6fe98173d4ba5e0f1c77c01bf1b4
zzpz <michael.wilson1@uqconnect.edu.au>
2021-09-24 18:49:45 +1000

    update profile image functional

--- backend/app/api/routes/images.py
+++ backend/app/api/routes/images.py
@@ -13,6 +13,8 @@ from app.api.dependencies.posts import (
     get_post_id_from_path,
     validate_post_modification_permissons,
 )
+from app.db.repositories.profiles import ProfilesRepository
+from app.models import profile
 
 
 # models
@@ -29,18 +31,49 @@ router = APIRouter()
 
 
 @router.post("/profile")
-async def upload_profile_image(
+async def update_profile_image(
     current_user: UserInDB = Depends(get_current_user),
     image: UploadFile = File(...),  # image as a streamable file
     image_repo: ImagesRepository = Depends(
         get_repository(ImagesRepository)
     ),  # images repo
+    profiles_repo: ProfilesRepository = Depends(get_repository(ProfilesRepository)),
 ) -> str:
     """
+    # UNTESTED
     Takes a file as input (assumed to be an image).
 
     Requests a file id from FILESTORE using image_service
     """
+
+    # profile repo and return new image? or just return a whole new public user?
+    profile = await profiles_repo.get_profile_by_user_id(user_id=current_user.id)
+
+    fid = profile.image
+
+    # here we SHOULD just overwrite if it already exists in FS...
+    if fid == "static_profile_default":
+        # upload the image
+        newfid = image_service.upload_image(image=image)
+        # update the database reference
+        await image_repo.update_fid(id_holder=current_user, newfid=newfid)
+
+        # delete old image not necessary
+        # image_service.delete_image(fid=fid)
+    else:
+        newfid = image_service.upload_image(image=image)
+        await image_repo.update_fid(id_holder=current_user, newfid=newfid)
+        image_service.delete_image(fid=fid)
+
+    # no easy 'update?' we basically delete and re-upload.. seems poor
+    # I DONT WANT TO UPDATE THE DATABASE EVERYTIME.
+    # JUST SAVE THE ID ONCE AND CHANGE WHAT THE URL REFERENCES
+
+    # return public_url
+    public_url = image_service.get_public_image_url(fid=newfid)
+
+    return public_url
+
     return "app.com/volume,fileID"
 
 
@@ -50,7 +83,7 @@ async def upload_profile_image(
     status_code=status.HTTP_201_CREATED,
     # dependencies=[Depends(validate_post_modification_permissons)],
 )
-async def upload_post_image(  # technically 'update post image'
+async def update_post_image(  # technically 'update post image'
     image: UploadFile = File(...),
     post: PostInDB = Depends(get_post_id_from_path),  # posts dependency
     image_repo: ImagesRepository = Depends(
