85372edabecf43f08168ab5abe3afa76464ac96b
stevoAbr <stephan68abr@gmail.com>
2021-10-16 23:17:22 +1000

    Route Priority Issue fix

--- backend/app/api/routes/profiles.py
+++ backend/app/api/routes/profiles.py
@@ -16,6 +16,27 @@ router = APIRouter()
 
 
 @router.get(
+    "/me/",
+    response_model=ProfilePublic,
+    name="profiles:get-current-user-profile",
+)
+async def get_own_profile(
+    current_user: UserInDB = Depends(get_current_user),
+    profiles_repo: ProfilesRepository = Depends(get_repository(ProfilesRepository)),
+) -> ProfilePublic:
+    """
+    # UNTESTED
+
+    Return the current user's profile.
+    """
+
+    uid = current_user.id
+    profile = await profiles_repo.get_profile_by_user_id(user_id=uid)
+
+    return profile
+    
+
+@router.get(
     "/{user_id}/",  # this seems bad unless we use UUID.
     response_model=ProfilePublic,
     name="profiles:get-profile-by-id",
@@ -37,27 +58,6 @@ async def get_profile_by_user_id(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No profile found for that user.",
         )
-
-    return profile
-
-
-@router.get(
-    "/me/",
-    response_model=ProfilePublic,
-    name="profiles:get-current-user-profile",
-)
-async def get_own_profile(
-    current_user: UserInDB = Depends(get_current_user),
-    profiles_repo: ProfilesRepository = Depends(get_repository(ProfilesRepository)),
-) -> ProfilePublic:
-    """
-    # UNTESTED
-
-    Return the current user's profile.
-    """
-
-    uid = current_user.id
-    profile = await profiles_repo.get_profile_by_user_id(user_id=uid)
 
     return profile
 
